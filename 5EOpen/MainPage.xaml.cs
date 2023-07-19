176403ddf1f11a5f4288060d7c3b5e925edbec5c
Alex VanLaningham <77026821+ajvanlaningham@users.noreply.github.com>
2023-07-19 15:37:19 -0400

    FEATURE: Functionality for french language/localization

--- 5EOpen/MainPage.xaml.cs
+++ 5EOpen/MainPage.xaml.cs
@@ -1,22 +1,36 @@
-﻿namespace _5EOpen;
+﻿using _5EOpen.Resources.ResX;
+using LocalizationResourceManager.Maui;
+using System.Globalization;
+
+namespace _5EOpen;
 
 public partial class MainPage : ContentPage
 {
 	int count = 0;
+	LocalizationResourceManager.Maui.LocalizedString _counterClicked;
 
-	public MainPage()
+	ILocalizationResourceManager _localizationResourceManager;
+
+	public MainPage(ILocalizationResourceManager localizationResourceManager)
 	{
 		InitializeComponent();
+		_localizationResourceManager = localizationResourceManager;
+		_counterClicked = new (() =>_localizationResourceManager["Counter"]);
 	}
 
 	private void OnCounterClicked(object sender, EventArgs e)
 	{
+		if (_localizationResourceManager.CurrentCulture.TwoLetterISOLanguageName == "fr")
+		{
+            _localizationResourceManager.CurrentCulture = new CultureInfo("en");
+        }
+		else
+		{ 
+			_localizationResourceManager.CurrentCulture = new CultureInfo("fr");
+		}
 		count++;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+		CounterBtn.Text = string.Format(_counterClicked.Localized, count);
 
 		SemanticScreenReader.Announce(CounterBtn.Text);
 	}
--- 5EOpen/MauiProgram.cs
+++ 5EOpen/MauiProgram.cs
@@ -24,6 +24,7 @@ public static class MauiProgram
 
 #if DEBUG
 		builder.Logging.AddDebug();
+		builder.Services.AddTransient<MainPage>();
 #endif
 
 		return builder.Build();
